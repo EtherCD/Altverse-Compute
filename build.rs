046ab12b0292cc6fa286146d267fa1c31a3fa9bc
EtherCD <ethernetcoder@gmail.com>
2025-12-24 18:11:23 +0200

    Submodule proto

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
   napi_build::setup();
 
-  let proto_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("protos");
+  let proto_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/proto/proto");
   let proto_file = proto_dir.join("game.proto");
 
   println!("cargo:rerun-if-changed={}", proto_dir.display());
